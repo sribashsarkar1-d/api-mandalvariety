e881cc34942dc4b46e74a13f21395ec99e8cb4dd
sribash sarkar <sribashsarkarblp@gmail.com>
2026-05-24 02:32:40 +0530

    admin products/list.php update commit

--- admin/check_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;charset=utf8mb4", "root", "");
-    $stmt = $conn->query("SHOW DATABASES");
-    $dbs = $stmt->fetchAll(PDO::FETCH_COLUMN);
-    print_r($dbs);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
